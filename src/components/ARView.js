a00c585d82efc1ded0f01bd2b914238ad882232d
crystalhoo <crystal.hoo8@gmail.com>
2020-07-25 06:38:02 +0800

    update AR view

--- src/components/ARView.js
+++ src/components/ARView.js
@@ -1,23 +1,19 @@
 import React, { Component } from "react";
-
+import "./../ar.css";
 import model from "../assets/floorPlan.glb";
 
 export default class ARView extends Component {
   componentDidMount() {
     //A-SCENE
     let aScene = document.createElement("a-scene");
-    aScene.setAttribute("arjs", "");
-
+    aScene.setAttribute("arjs", "debugUIEnabled: false");
     //A-MARKER
     let aMarker = document.createElement("a-marker-camera");
     aMarker.setAttribute("preset", "hiro");
-
     let aChair = document.createElement("a-entity");
-
     aChair.setAttribute("position", "0 0 0");
     aChair.setAttribute("scale", "0.005 0.003 0.003");
     aChair.setAttribute("gltf-model", `${model}`);
-
     aMarker.appendChild(aChair);
     aScene.appendChild(aMarker);
     document.body.appendChild(aScene);
